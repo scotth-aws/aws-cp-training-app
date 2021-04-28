9da11d39a287f6e7dcec785858f0e09af74ddea6
Hendrickson <hesct@3c22fbcc21f2.ant.amazon.com>
2021-04-28 16:11:40 -0400

    end of array now 50

--- src/App.js
+++ src/App.js
@@ -120,9 +120,11 @@ class App extends Component {
     var q = ranNums[ranNumsIndex];
     console.log(TAG, '_getAQuestion ranNumsIndex ' + ranNumsIndex);
     ranNumsIndex += 1;
-    if (ranNumsIndex === 50)
+    if (ranNumsIndex === 50) {
       ranNumsIndex = 1;
       ranNums = this._shuffle(qArray);
+      q = ranNums[ranNumsIndex];
+    }
 
     const apiName = 'CPTraining';
     const path = "/";
