c2576a562c9ae399aad1717567820232c51d9a7c
Hendrickson <hesct@3c22fbcc21f2.ant.amazon.com>
2021-05-04 09:23:07 -0400

    added 18 more questions

--- src/Content.js
+++ src/Content.js
@@ -19,7 +19,7 @@ const TAG = 'Content';
 
 var ranNums;
 var ranNumsIndex = 1;
-var qArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50];
+var qArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68];
 Amplify.configure({
 
   API: {
@@ -104,7 +104,7 @@ class Content extends React.Component {
     var q = ranNums[ranNumsIndex];
     console.log(TAG, '_getAQuestion ranNumsIndex ' + ranNumsIndex);
     ranNumsIndex += 1;
-    if (ranNumsIndex === 50) {
+    if (ranNumsIndex === 68) {
       alert('You have reached the end of the questions. What do you think your score is? 🤯')
       ranNumsIndex = 1;
       ranNums = this._shuffle(qArray);
